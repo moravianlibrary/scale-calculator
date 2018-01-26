6a88b7e93f3b524af6a8178066a4a457d6d966b7
zabak <zabak@users.noreply.github.com>
2018-01-26 11:25:48 +0100

    Update texts.js
    minor edits

--- texts.js
+++ texts.js
@@ -25,7 +25,7 @@ var texts = {
     "en": "System"
   },
   "help-text": {
-    "cs": "<p>Aplikace slouží k rychlému zjištění měřítka na starých mapách. Lze ji využít k převodu měřítka zapsaného ve starších formátech za použití historických délkových měr i metrické soustavy do tvaru 1:M.</p> <p>Nejčastěji se na starých – rukopisných i tištěných – mapách setkáváme s dvojí podobou údaje o měřítku: není zapsán vůbec nebo je vyjádřen úsečkou. Aplikace umožňuje výpočet měřítka v obou případech. Jde vždy o výpočet orientační, který slouží zejména knihovníkům a archivářům spravujícím a zpřístupňujícím formou katalogu či jiné pomůcky historické mapové fondy, historickým kartografům i laickým zájemcům o staré mapy. Proto je vhodné přiměřeně zaokrouhlovat. Aplikace nenahrazuje kartometrickou analýzu mapy provedenou pokročilými kartografickými software.<p> <p>Aplikace pracuje s jednotkami užívanými ve Střední Evropě v období raného novověku až po metrickou soustavu: bere v potaz české délkové jednotky užívané do roku 1765, moravské užívané do roku 1758, slezské užívané do roku 1750, pruské z 18. století, slovensko-uherské (jak z 18., tak z 19. století), dolnorakouskou soustavu zaváděnou v jednotlivých zmíněných zemích od druhé poloviny 18. století a metrickou soustavu zavedenou v Rakousku-Uhersku zákonem z roku 1871, který vstoupil v platnost 1. 1. 1876.</p> <p>Vycházeli jsme z následující literatury: Hlaváček Ivan a kol., Vademecum pomocných věd historických, 3. vyd., Praha 2002; Hofmann Gustav, Metrologická příručka pro Čechy, Moravu a Slezsko do zavedení metrické soustavy, Plzeň-Sušice 1984; Húščava Alexander, Poľnohospodárske miery na Slovensku, Bratislava 1972.</p> <p>Autoři: Eva Chodějovská, Petr Žabička, Erich Duda.<br> Moravská zemská knihovna 2018.</p> <p> <strong>Nápověda</strong><br> Když není měřítko na mapě uvedeno: <ol> <li>navolíme v prvním poli soustavu jednotek, kterou předpokládáme, že autor mapy užil,</li> <li>změříme vzdálenost mezi dvěma vybranými body na staré mapě (v milimetrech) a zapíšeme do druhého pole,</li> <li>vyznačíme stejné dva body na současné mapě zde níže,</li> <li>údaj o vzdálenosti se automaticky přenese a bude zapsán v kilometrech ve třetím poli a</li> <li>získáme údaj 1: …</ol> </ul> </p> <p> Když je na mapě grafické měřítko, tedy úsečka: <ol> <li>navolíme v prvním poli soustavu jednotek, kterou autor mapy užil,</li> <li>změříme délku úsečky na mapě v milimetrech a zapíšeme do druhého pole,</li> <li>zapíšeme počet jednotek do třetího pole,</li> <li>navolíme jednotky, které autor mapy užil, ve čtvrtém poli a</li> <li>získáme údaj 1: …</li> <ol> </p>",
+    "cs": "<p>Aplikace slouží k rychlému zjištění měřítka na starých mapách. Lze ji využít k převodu měřítka zapsaného ve starších formátech za použití historických délkových měr i metrické soustavy do tvaru 1:M.</p> <p> <strong>Nápověda</strong><br> Když není měřítko na mapě uvedeno: <ol> <li>navolíme v prvním poli soustavu jednotek, kterou předpokládáme, že autor mapy užil,</li> <li>změříme vzdálenost mezi dvěma vybranými body na staré mapě (v milimetrech) a zapíšeme do druhého pole,</li> <li>vyznačíme stejné dva body na současné mapě zde níže,</li> <li>údaj o vzdálenosti se automaticky přenese a bude zapsán v kilometrech ve třetím poli a</li> <li>získáme údaj 1: …</ol> </ul> </p> <p> Když je na mapě grafické měřítko, tedy úsečka: <ol> <li>navolíme v prvním poli soustavu jednotek, kterou autor mapy užil,</li> <li>změříme délku úsečky na mapě v milimetrech a zapíšeme do druhého pole,</li> <li>zapíšeme počet jednotek do třetího pole,</li> <li>navolíme jednotky, které autor mapy užil, ve čtvrtém poli a</li> <li>získáme údaj 1: …</li> <ol> </p>  <strong>O měřítcích starých map</strong>  <p>Nejčastěji se na starých – rukopisných i tištěných – mapách setkáváme s dvojí podobou údaje o měřítku: není zapsán vůbec nebo je vyjádřen úsečkou. Aplikace umožňuje výpočet měřítka v obou případech. Jde vždy o výpočet orientační, který slouží zejména knihovníkům a archivářům spravujícím a zpřístupňujícím formou katalogu či jiné pomůcky historické mapové fondy, historickým kartografům i laickým zájemcům o staré mapy. Proto je vhodné přiměřeně zaokrouhlovat. Aplikace nenahrazuje kartometrickou analýzu mapy provedenou pokročilými kartografickými software.<p> <p>Aplikace pracuje s jednotkami užívanými ve Střední Evropě v období raného novověku až po metrickou soustavu: bere v potaz české délkové jednotky užívané do roku 1765, moravské užívané do roku 1758, slezské užívané do roku 1750, pruské z 18. století, slovensko-uherské (jak z 18., tak z 19. století), dolnorakouskou soustavu zaváděnou v jednotlivých zmíněných zemích od druhé poloviny 18. století a metrickou soustavu zavedenou v Rakousku-Uhersku zákonem z roku 1871, který vstoupil v platnost 1. 1. 1876.</p> <p>Vycházeli jsme z následující literatury: Hlaváček Ivan a kol., Vademecum pomocných věd historických, 3. vyd., Praha 2002; Hofmann Gustav, Metrologická příručka pro Čechy, Moravu a Slezsko do zavedení metrické soustavy, Plzeň-Sušice 1984; Húščava Alexander, Poľnohospodárske miery na Slovensku, Bratislava 1972.</p> <p>Autoři: Eva Chodějovská, Petr Žabička, Erich Duda.<br> Moravská zemská knihovna 2018.</p> ",
     "de": "<h3>Grafischer Maßstab auf die Altkarte</h3><p>Die Strecke XXXX-Millimeter lang ist XXXX<br>beträgt<br>1: …</p><h3>Keine Maßstabangabe auf die Altkarte</h3><p>Die Distanz zwischen zwei ausgewählten Punkten auf die Altkarte beträgt XXXX Millimeter, die Distanz zwischen derselben Punkten auf die gegenwärtige Karte hier unten (die Punkten sind zu markieren) beträgt XXXX,<br>beträgt<br>1: …</p>",
     "en": "<h3>Graphic scale on the antique map</h3><p>line segment XXXX mm long is XXXX<br>which makes<br>1: …</p><h3>No scale on the antique map</h3><p>Distance between two selected points on the antique map is XXXX mm; distance between the same points on the current map is (mark them in the map bellow) XXXX mm<br>which makes<br>1: …</p>"
   },
@@ -37,7 +37,7 @@ var texts = {
   "ve-skutecnosti": {
     "cs": "ve skutečnosti.",
     "de": "auf dem Boden.",
-    "en": "on a ground."
+    "en": "on the ground."
   },
   "ceska-1765": {
     "cs": "česká (do 1765)",
@@ -47,32 +47,32 @@ var texts = {
   "mile": {
     "cs": "míle - böhmische Meile",
     "de": "böhmische Meile",
-    "en": "böhmische Meile"
+    "en": "mile - böhmische Meile"
   },
   "provazec": {
     "cs": "provazec - böhmische Seile",
     "de": "böhmische Seile",
-    "en": "böhmische Seile"
+    "en": "rope - böhmische Seile"
   },
   "sah": {
     "cs": "sáh - böhmischer Klafter",
     "de": "böhmischer Klafter",
-    "en": "böhmischer Klafter"
+    "en": "fathom - böhmischer Klafter"
   },
   "loket": {
     "cs": "loket - bömische Elle",
     "de": "bömische Elle",
-    "en": "bömische Elle"
+    "en": "cubit - bömische Elle"
   },
   "stopa": {
     "cs": "stopa - böhmischer Schuh",
     "de": "böhmischer Schuh",
-    "en": "böhmischer Schuh"
+    "en": "feet - böhmischer Schuh"
   },
   "palec": {
     "cs": "palec - böhmischer Zoll",
     "de": "böhmischer Zoll",
-    "en": "böhmischer Zoll"
+    "en": "inch - böhmischer Zoll"
   },
   "dolnorakouska": {
     "cs": "dolnorakouská - niederösterreichische",
@@ -82,27 +82,27 @@ var texts = {
   "mile-dolnorakouska": {
     "cs": "míle dolnorakouská - niederösterreichische Meile",
     "de": "niederösterreichische Meile",
-    "en": "niederösterreichische Meile"
+    "en": "mile - niederösterreichische Meile"
   },
   "sah-dolnorakousky": {
     "cs": "sáh dolnorakouský - niederösterreichischer Klafter",
     "de": "niederösterreichischer Klafter",
-    "en": "niederösterreichischer Klafter"
+    "en": "fathom - niederösterreichischer Klafter"
   },
   "loket-dolnorakousky": {
     "cs": "loket dolnorakouský - niederösterreichische Elle",
     "de": "niederösterreichische Elle",
-    "en": "niederösterreichische Elle"
+    "en": "cubit - niederösterreichische Elle"
   },
   "stopa-dolnorakouska": {
     "cs": "stopa dolnorakouská - niederösterreichischer Schuh",
     "de": "niederösterreichischer Schuh",
-    "en": "niederösterreichischer Schuh"
+    "en": "feet - niederösterreichischer Schuh"
   },
   "palec-dolnorakousky": {
     "cs": "palec dolnorakouský - niederösterreichischer Zoll",
     "de": "niederösterreichischer Zoll",
-    "en": "niederösterreichischer Zoll"
+    "en": "inch - niederösterreichischer Zoll"
   },
   "moravska-1758": {
     "cs": "moravská (do 1758)",
@@ -112,22 +112,22 @@ var texts = {
   "sah-moravsky": {
     "cs": "sáh moravský - mährischer Klafter",
     "de": "mährischer Klafter",
-    "en": "mährischer Klafter"
+    "en": "fathom - mährischer Klafter"
   },
   "loket-moravsky": {
     "cs": "loket moravský - mährische Elle",
     "de": "mährische Elle",
-    "en": "mährische Elle"
+    "en": "cubit - mährische Elle"
   },
   "stopa-moravska": {
     "cs": "stopa moravská - mährischer Schuh",
     "de": "mährischer Schuh",
-    "en": "mährischer Schuh"
+    "en": "feet - mährischer Schuh"
   },
   "palec-moravsky": {
     "cs": "palec moravský - mährischer Zoll",
     "de": "mährischer Zoll",
-    "en": "mährischer Zoll"
+    "en": "inch - mährischer Zoll"
   },
   "slezska-1750": {
     "cs": "slezská (do 1750)",
@@ -137,22 +137,22 @@ var texts = {
   "mile-slezska": {
     "cs": "míle slezská - schlesische Meile",
     "de": "schlesische Meile",
-    "en": "schlesische Meile"
+    "en": "mile - schlesische Meile"
   },
   "provazec-slezsky": {
     "cs": "provazec slezský - schlesische Seile",
     "de": "schlesische Seile",
-    "en": "schlesische Seile"
+    "en": "rope - schlesische Seile"
   },
   "sah-slezsky": {
     "cs": "sáh slezský - schlesischer Klafter",
     "de": "schlesischer Klafter",
-    "en": "schlesischer Klafter"
+    "en": "fathom - schlesischer Klafter"
   },
   "loket-slezsky": {
     "cs": "loket slezský - schlesische Elle",
     "de": "schlesische Elle",
-    "en": "schlesische Elle"
+    "en": "cubit - schlesische Elle"
   },
   "stopa-slezska": {
     "cs": "stopa slezská - schlesischer Schuh",
@@ -162,7 +162,7 @@ var texts = {
   "palec-slezsky": {
     "cs": "palec slezský - schlesischer Zoll",
     "de": "schlesischer Zoll",
-    "en": "schlesischer Zoll"
+    "en": "inch - schlesischer Zoll"
   },
   "pruska": {
     "cs": "pruská - preußische",
@@ -172,27 +172,27 @@ var texts = {
   "mile-pruska": {
     "cs": "míle pruská - preußische Meile",
     "de": "preußische Meile",
-    "en": "preußische Meile"
+    "en": "mile - preußische Meile"
   },
   "sah-prusky": {
     "cs": "sáh pruský - preußischer Klafter",
     "de": "preußischer Klafter",
-    "en": "preußischer Klafter"
+    "en": "fathom - preußischer Klafter"
   },
   "loket-prusky": {
     "cs": "loket pruský - preußische Elle",
     "de": "preußische Elle",
-    "en": "preußische Elle"
+    "en": "cubit - preußische Elle"
   },
   "stopa-pruska": {
     "cs": "stopa pruská - preußischer Schuh",
     "de": "preußischer Schuh",
-    "en": "preußischer Schuh"
+    "en": "feet - preußischer Schuh"
   },
   "palec-prusky": {
     "cs": "palec pruský - preußischer Zoll",
     "de": "preußischer Zoll",
-    "en": "preußischer Zoll"
+    "en": "inch - preußischer Zoll"
   },
   "metricka": {
     "cs": "metrická",
@@ -222,12 +222,12 @@ var texts = {
   "kralovsky-sah-slovensky": {
     "cs": "královský sáh slovenský - slowakischer königlicher Klafter",
     "de": "slowakischer königlicher Klafter",
-    "en": "slowakischer königlicher Klafter"
+    "en": "fathom - slowakischer königlicher Klafter"
   },
   "loket-slovensky": {
     "cs": "loket slovenský - slowakische Elle",
     "de": "slowakische Elle",
-    "en": "slowakische Elle"
+    "en": "inch - slowakische Elle"
   },
   "slovenska-uherska-19": {
     "cs": "slovenská-uherská (19. století)",
@@ -237,16 +237,16 @@ var texts = {
   "mile-uherska": {
     "cs": "míle uherská - ungarische Meile",
     "de": "ungarische Meile",
-    "en": "ungarische Meile"
+    "en": "mile - ungarische Meile"
   },
   "sah-slovensky": {
     "cs": "sáh slovenský - slowakischer Klafter",
     "de": "slowakischer Klafter",
-    "en": "slowakischer Klafter"
+    "en": "fathom - slowakischer Klafter"
   },
   "loket-slovensky": {
     "cs": "loket slovenský - slowakische Elle",
     "de": "slowakische Elle",
-    "en": "slowakische Elle"
+    "en": "cubit - slowakische Elle"
   },
 }
